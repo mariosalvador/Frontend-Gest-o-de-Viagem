40e68aa087e17bf5134f659f0ad6bfebd6f68c98
mariop06 <mariopaulobunga.com>
2024-08-04 11:23:40 +0100

    Initialized Travel Registration

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { ArrowRight, Calendar, MapPin } from "lucide-react"
 
 
 export const App = () => {
@@ -8,10 +9,20 @@ export const App = () => {
         <p>Convide seus amigos e planeje sua próxima viagem!</p>
 
         <div className="w-full h-16 flex items-center justify-between rounded-lg bg-zinc-900 px-4 shadow-shape" >
+          <div className="flex h-full gap-2 items-center">
+            <MapPin className="size-5 text-zinc-400" />
+            <input type="text" placeholder="Para onde você vai?" className="bg-transparent text-zinc-400 text-lg outline-none" />
+          </div>
 
-          <input type="text" placeholder="Para onde você vai?" className="bg-transparent text-zinc-400 text-lg outline-none" />
-          <input type="text" placeholder="Quando?" className="bg-transparent text-zinc-400 text-lg outline-none" />
-          <button>Continuar</button>
+          <div className="flex h-full gap-2 items-center" >
+            <Calendar className="size-5 text-zinc-400" />
+            <input type="text" placeholder="Quando?" className="bg-transparent text-zinc-400 text-lg outline-none" />
+          </div>
+
+          <button className="flex items-center rounded-lg text-lime-950 font-medium bg-lime-500 hover:bg-lime-400 p-5 h-9 gap-2" >
+            Continuar
+            <ArrowRight />
+          </button>
 
         </div>
 
